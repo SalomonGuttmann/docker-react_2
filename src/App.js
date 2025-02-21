a4363f6ca40b8cc2815610f108e5e1add5f696c2
040772 <salomonguttmann@gmail.com>
2025-02-21 14:24:40 +0100

    more changes in App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          I laufe auf GCP Salomon!
+          I running on Google Application Engine Salomon!
         </p>
         <a
           className="App-link"
